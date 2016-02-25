e27e43f123eb3e436591475f427e6c32a12cb8e2
Eray Diler <eray.diler@gmail.com>
2016-02-25 18:02:00 +0200

    Added a button to test feedback method.

--- Example/Tryouts/TRYViewController.m
+++ Example/Tryouts/TRYViewController.m
@@ -24,6 +24,36 @@
     self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 
     [self.view setBackgroundColor:[UIColor whiteColor]];
+
+    UIButton *feedbackButton = [UIButton buttonWithType:UIButtonTypeCustom];
+
+    [feedbackButton setFrame:CGRectMake((self.view.bounds.size.width / 2) - 50,
+                                        self.view.bounds.size.height / 2,
+                                        100,
+                                        25.0)];
+
+    [feedbackButton.layer setBorderWidth:1.0];
+
+    [feedbackButton setTitleColor:[UIColor blueColor]
+                         forState:UIControlStateNormal];
+
+    [feedbackButton setTitle:NSLocalizedString(@"Feedback", nil)
+                    forState:UIControlStateNormal];
+
+    [feedbackButton setTitleColor:[[UIColor blueColor] colorWithAlphaComponent:0.5]
+                         forState:UIControlStateHighlighted];
+
+    [feedbackButton addTarget:self
+                       action:@selector(didTapFeedbackButton:)
+             forControlEvents:UIControlEventTouchUpInside];
+
+    [self.view addSubview:feedbackButton];
+}
+
+#pragma mark - Actions
+
+- (void)didTapFeedbackButton:(id)sender {
+    NSLog(@"GIVE FEEDBACK");
 }
 
 @end
