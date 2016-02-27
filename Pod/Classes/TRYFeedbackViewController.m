55fc14abdd18aa2426725a2b888d1e3f10c43280
Eray Diler <eray.diler@gmail.com>
2016-02-27 23:47:56 +0200

    Added background image and icon into example project’s imageset temporarily.

--- Pod/Classes/TRYFeedbackViewController.m
+++ Pod/Classes/TRYFeedbackViewController.m
@@ -15,11 +15,26 @@
     [self.view setBackgroundColor:[UIColor whiteColor]];
 
 
-    UIImageView *tryoutsIcon = [[UIImageView alloc] initForAutoLayout];
+    UIImageView *backgroundView = [UIImageView newAutoLayoutView];
+
+    [backgroundView setImage:[UIImage imageNamed:@"bg-feedback"]];
+    [backgroundView.layer setBorderWidth:1.0];
+    [backgroundView.layer setBorderColor:[UIColor redColor].CGColor];
+
+    [self.view addSubview:backgroundView];
+
+    [backgroundView autoSetDimension:ALDimensionWidth toSize:180.0];
+    [backgroundView autoSetDimension:ALDimensionHeight toSize:211.0];
+    [backgroundView autoCenterInSuperview];
+
+
+    UIImageView *tryoutsIcon = [UIImageView newAutoLayoutView];
 
     [tryoutsIcon setImage:[UIImage imageNamed:@"icon-tryouts"]];
+    [tryoutsIcon.layer setBorderWidth:1.0];
+    [tryoutsIcon.layer setBorderColor:[UIColor greenColor].CGColor];
 
-    [self.view addSubview:tryoutsIcon];
+    [backgroundView addSubview:tryoutsIcon];
 
     [tryoutsIcon autoSetDimension:ALDimensionWidth toSize:50.0];
     [tryoutsIcon autoSetDimension:ALDimensionHeight toSize:50.0];
