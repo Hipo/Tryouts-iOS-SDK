1d7a4e91b8c0129a8fcc4b00b6a1b4cc48abe110
Eray Diler <eray.diler@gmail.com>
2016-03-24 18:12:34 +0200

    Added new overlayView class into project.

---
+++ Pod/Classes/Views/TRYFeedbackOverlayView.h
@@ -0,0 +1,13 @@
+//
+//  TRYFeedbackOverlayView.h
+//  Pods
+//
+//  Created by Eray on 24/03/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRYFeedbackOverlayView : UIView
+
+@end
---
+++ Pod/Classes/Views/TRYFeedbackOverlayView.m
@@ -0,0 +1,13 @@
+//
+//  TRYFeedbackOverlayView.m
+//  Pods
+//
+//  Created by Eray on 24/03/16.
+//
+//
+
+#import "TRYFeedbackOverlayView.h"
+
+@implementation TRYFeedbackOverlayView
+
+@end
