b68c3b7c656b779f81341c68afcc13dfdd9ba2f9
Eray Diler <eray.diler@gmail.com>
2016-04-07 12:57:13 +0300

    Added close button to feed back overlay view.

--- Pod/Classes/Views/TRYFeedbackOverlayView.m
+++ Pod/Classes/Views/TRYFeedbackOverlayView.m
@@ -10,14 +10,59 @@
 
 @implementation TRYFeedbackOverlayView
 
+#pragma mark - Init
+
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
 
     if (self) {
-        self.backgroundColor = [UIColor whiteColor];
+        [self configureLayout];
     }
 
     return self;
+}
+
+#pragma mark - Layout
+
+- (void)configureLayout {
+    self.backgroundColor = [UIColor whiteColor];
+
+    UIButton *closeButton = [UIButton buttonWithType:UIButtonTypeCustom];
+
+    [closeButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+
+    [closeButton setImage:[UIImage imageNamed:@"bt-close"]
+                 forState:UIControlStateNormal];
+
+    [closeButton addTarget:self
+                    action:@selector(didTapCloseButton:)
+          forControlEvents:UIControlEventTouchUpInside];
+
+    NSMutableDictionary *views = [[NSMutableDictionary alloc]
+                                  initWithDictionary:@{ @"closeButton":closeButton }];
+
+    [self addSubview:closeButton];
+
+    NSArray *horizontalConstraints = [NSLayoutConstraint
+                                      constraintsWithVisualFormat:@"H:[closeButton]-10-|"
+                                      options:NSLayoutFormatAlignAllTop
+                                      metrics:nil
+                                      views:views];
+
+    NSArray *verticalConstraints = [NSLayoutConstraint
+                                    constraintsWithVisualFormat:@"V:|-10-[closeButton]"
+                                    options:NSLayoutFormatAlignAllTop
+                                    metrics:nil
+                                    views:views];
+
+    [NSLayoutConstraint activateConstraints:horizontalConstraints];
+    [NSLayoutConstraint activateConstraints:verticalConstraints];
+}
+
+#pragma mark - Actions
+
+- (void)didTapCloseButton:(id)sender {
+    NSLog(@"TAPPED");
 }
 
 @end
