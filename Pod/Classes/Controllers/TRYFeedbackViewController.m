790241a7ef45e617af1ad07a5c758c6747fef1d0
Eray Diler <eray.diler@gmail.com>
2016-04-12 16:35:59 +0300

    Added ability to get the screenshot for current window.

--- Pod/Classes/Controllers/TRYFeedbackViewController.m
+++ Pod/Classes/Controllers/TRYFeedbackViewController.m
@@ -9,7 +9,12 @@
 #import "TRYFeedbackViewController.h"
 #import "TRYFeedbackOverlayView.h"
 
-@interface TRYFeedbackOverlayView () <TRYFeedbackOverlayViewDelegate>
+@interface TRYFeedbackViewController () <TRYFeedbackOverlayViewDelegate>
+
+@property (nonatomic, strong) NSString *encodedScreenshot;
+
+- (UIImage *)screenShotOfWindow;
+- (NSString *)encodedStringOfScreenshot;
 
 @end
 
@@ -22,6 +27,8 @@
                                      initWithTarget:self
                                              action:@selector(didTapBackground:)]];
 
+    _encodedScreenshot = [self encodedStringOfScreenshot];
+
     [self configureView];
 }
 
@@ -30,6 +37,7 @@
 - (void)configureView {
     self.view.backgroundColor = [UIColor clearColor];
 
+    [self screenShotOfWindow];
 
     TRYFeedbackOverlayView *feedBackOverlayView = [[TRYFeedbackOverlayView alloc]
                                                    initWithFrame:self.view.bounds];
@@ -52,6 +60,40 @@
 
 - (void)feedbackOverlayViewDidTapSubmitButton:(TRYFeedbackOverlayView *)feedbackOverlayView {
     NSLog(@"Triggered");
+}
+
+#pragma mark - Screen shot
+
+- (UIImage *)screenShotOfWindow {
+    // Create graphics context with screen size
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+
+    UIGraphicsBeginImageContext(screenRect.size);
+
+    CGContextRef currentContex = UIGraphicsGetCurrentContext();
+
+    [[UIColor blackColor] set];
+    CGContextFillRect(currentContex, screenRect);
+
+    // Referance to current window
+    UIWindow *window = [UIApplication sharedApplication].keyWindow;
+
+    // Transfering content into the context
+    [window.layer renderInContext:currentContex];
+
+    UIImage *screenshotImage = UIGraphicsGetImageFromCurrentImageContext();
+
+    UIGraphicsEndImageContext();
+
+    return screenshotImage;
+}
+
+- (NSString *)encodedStringOfScreenshot {
+    UIImage *screenshotImage = [self screenShotOfWindow];
+
+    NSData *imageData = UIImageJPEGRepresentation(screenshotImage, 1.0);
+
+    return [imageData base64Encoding];
 }
 
 @end
